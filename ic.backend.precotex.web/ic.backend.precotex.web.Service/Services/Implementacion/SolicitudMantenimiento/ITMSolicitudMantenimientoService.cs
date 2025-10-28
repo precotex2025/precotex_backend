c5ace2f35f67752e67dbfe5b96874fcc6287271d
dayala99 <117493354+dayala99@users.noreply.github.com>
2025-10-28 13:01:17 -0500

    Vista Visor

--- ic.backend.precotex.web/ic.backend.precotex.web.Service/Services/Implementacion/SolicitudMantenimiento/ITMSolicitudMantenimientoService.cs
+++ ic.backend.precotex.web/ic.backend.precotex.web.Service/Services/Implementacion/SolicitudMantenimiento/ITMSolicitudMantenimientoService.cs
@@ -13,5 +13,8 @@ namespace ic.backend.precotex.web.Service.Services.Implementacion.SolicitudMante
         Task<ServiceResponse<int>> ProcesoMntoSolicitudMantenimiento(TM_Solicitud_Mantenimiento tM_Solicitud_Mantenimiento, string sTipoTransac);
         Task<ServiceResponseList<TM_Maquina>?> ObtieneInformacionMaquinas(string sCodMaquina);
         Task<ServiceResponseList<TM_Solicitud_Mantenimiento>?> ObtieneInformacionSolicitudMantenimiento(DateTime FecIni, DateTime FecFin, string codUsuario);
+
+        /*VISOR*/
+        Task<ServiceResponseList<TM_Solicitud_Mantenimiento>?> ObtieneInformacionSolicitudesVisor();
     }
 }
