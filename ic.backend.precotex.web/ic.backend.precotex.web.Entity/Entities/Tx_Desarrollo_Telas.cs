c8964af99529c0656ef9f97926d436cdf0f4ff43
hmedina1402-Prx <programer.omega@gmail.com>
2025-12-10 11:09:39 -0500

    feat: Desarrollo Tela Agrega Ruta_Archivo_Ant
    feat: Desarrollo Tela Agrega Ruta_Archivo_Ant

--- ic.backend.precotex.web/ic.backend.precotex.web.Entity/Entities/Tx_Desarrollo_Telas.cs
+++ ic.backend.precotex.web/ic.backend.precotex.web.Entity/Entities/Tx_Desarrollo_Telas.cs
@@ -16,6 +16,7 @@ namespace ic.backend.precotex.web.Entity.Entities
         public string? Nom_Version { get; set; }
         public string? Comentario { get; set; }
         public string? Ruta_Archivo { get; set; }
+        public string? Ruta_Archivo_Ant { get; set; }
         public DateTime? Fec_Registro_Solicitud { get; set; }
         public string? Cod_Usuario_Solicitud { get; set; }
     }
