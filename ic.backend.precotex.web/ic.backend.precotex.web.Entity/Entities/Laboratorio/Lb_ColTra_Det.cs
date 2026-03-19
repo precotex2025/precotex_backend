776731881a145d77f64eb6a38301c386b41f0c40
dayala99 <117493354+dayala99@users.noreply.github.com>
2026-03-19 10:58:58 -0500

    Add variable entity Lb_ColaTra_Des

--- ic.backend.precotex.web/ic.backend.precotex.web.Entity/Entities/Laboratorio/Lb_ColTra_Det.cs
+++ ic.backend.precotex.web/ic.backend.precotex.web.Entity/Entities/Laboratorio/Lb_ColTra_Det.cs
@@ -48,6 +48,7 @@ namespace ic.backend.precotex.web.Entity.Entities.Laboratorio
         public decimal? Ph_Jab3 { get; set; }
         public decimal? SulfatoReal { get; set; }
         public string? Procedencia { get; set; }
+        public string? Fec_Ent { get; set; }
 
         //PRODUCCION
         public string? Cod_OrdTra { get; set; }
