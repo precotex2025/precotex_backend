39ee8bd7151a0f7803846319c0b19518c25071eb
hmedina1402-Prx <programer.omega@gmail.com>
2026-03-31 09:42:46 -0500

    feat: Avance Copia Sofidya
    feat: Avance Copia Sofidya

---
+++ ic.backend.precotex.web/ic.backend.precotex.web.Service/Services/SecureNorm/SNDocumentosControladosService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ic.backend.precotex.web.Service.Services.SecureNorm
+{
+    class SNDocumentosControladosService
+    {
+    }
+}
