aefbfc46333b3222bc703e5155218b6d8ce43e91
CiscO1548 <huamaniangulofranciscojavier@gmail.com>
2026-08-20 08:49:40 -0500

    Agregados y cambios del SecureSoma

--- ic.backend.precotex.web/ic.backend.precotex.web.Data/Repositories/Implementation/SecureNorm/ISNAuditoriaRepository.cs
+++ ic.backend.precotex.web/ic.backend.precotex.web.Data/Repositories/Implementation/SecureNorm/ISNAuditoriaRepository.cs
@@ -8,5 +8,7 @@ namespace ic.backend.precotex.web.Data.Repositories.Implementation.SecureNorm
     {
         Task<(int Codigo, string Mensaje)> ProcesoMnto(SN_Auditoria sN_Auditoria, string sTipoTransac);
         Task<IEnumerable<SN_Auditoria>?> Listado(string sFiltro);
+        Task<IEnumerable<SN_Auditoria_Ejecucion>?> ListadoEjecucion(string sFiltro);
+        Task<(int Codigo, string Mensaje)> ProcesoMntoEjecucion(SN_Auditoria_Ejecucion ejecucion, string sTipoTransac);
     }
 }
--- ic.backend.precotex.web/ic.backend.precotex.web.Service/Services/Implementacion/SecureNorm/ISNAuditoriaService.cs
+++ ic.backend.precotex.web/ic.backend.precotex.web.Service/Services/Implementacion/SecureNorm/ISNAuditoriaService.cs
@@ -1,5 +1,6 @@
 using ic.backend.precotex.web.Entity.Entities.SecureNorm;
 using ic.backend.precotex.web.Service.common;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ic.backend.precotex.web.Service.Services.Implementacion.SecureNorm
@@ -8,5 +9,7 @@ namespace ic.backend.precotex.web.Service.Services.Implementacion.SecureNorm
     {
         Task<ServiceResponse<int>> ProcesoMnto(SN_Auditoria sN_Auditoria, string sTipoTransac);
         Task<ServiceResponseList<SN_Auditoria>?> Listado(string sFiltro);
+        Task<ServiceResponseList<SN_Auditoria_Ejecucion>?> ListadoEjecucion(string sFiltro);
+        Task<ServiceResponse<int>> ProcesoMntoEjecucion(SN_Auditoria_Ejecucion ejecucion, string sTipoTransac);
     }
 }
