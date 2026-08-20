a4bed3b9dadcafa77cf71dcd2b1acdf3d67347f1
jhon <calle.jhon.1035@gmail.com>
2026-08-20 17:45:45 -0500

    Hoja de formulacion y Reporte: Se agrego parametros para partidas de baño ciego

--- ic.backend.precotex.web/ic.backend.precotex.web.Entity/Entities/Laboratorio/Lb_Reporte.cs
+++ ic.backend.precotex.web/ic.backend.precotex.web.Entity/Entities/Laboratorio/Lb_Reporte.cs
@@ -59,6 +59,7 @@ namespace ic.backend.precotex.web.Entity.Entities.Laboratorio
         public string? Des_Colorante_Optico { get; set; }
         public string? Des_Concentracion { get; set; }
         public string? Fij_Tip_Des { get; set; }
+        public string? Partida_Bano_Ciego { get; set; }
 
     }
 
